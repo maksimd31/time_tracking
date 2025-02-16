e75a842f508c148769544eef06c5108681e96b00
mb <maksimdis31@gmail.com>
2025-02-16 11:46:13 +0300

    add remember_me

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class SignUpForm(UserCreationForm):
@@ -18,3 +19,19 @@ class SignUpForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 # widget=forms.PasswordInput()
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=100, label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя пользователя'}))
+
+    password = forms.CharField(max_length=100, label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
+
+    remember_me = forms.BooleanField(
+        required=False,
+        label="Запомнить меня",
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'remember_me']
