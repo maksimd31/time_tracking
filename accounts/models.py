0c0c14d1ecb57c147a5f542b6bacbcab0292120a
ms <maksimdis31@gmail.com>
2025-03-19 17:59:05 +0300

    fix bug avatar

--- accounts/models.py
+++ accounts/models.py
@@ -1,23 +1,46 @@
+
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
-
+from django.core.validators import FileExtensionValidator
+from django.urls import reverse
+from services.utils import unique_slugify
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    slug = models.SlugField(verbose_name='URL', max_length=255, blank=True, unique=True)
+    avatar = models.ImageField(
+        verbose_name='Аватар',
+        upload_to='images/avatars/%Y/%m/%d/',
+        default='images/avatars/default.jpg',
+        blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=('png', 'jpg', 'jpeg','dmg'))])
+    bio = models.TextField(max_length=500, blank=True, verbose_name='Информация о себе')
+    birth_date = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
 
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
-    bio = models.TextField()
+    class Meta:
+        """
+        Сортировка, название таблицы в базе данных
+        """
+        ordering = ('user',)
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
 
     def save(self, *args, **kwargs):
+        """
+        Сохранение полей модели при их отсутствии заполнения
+        """
+        if not self.slug:
+            self.slug = unique_slugify(self, self.user.username, self.slug)
         super().save(*args, **kwargs)
 
-        img = Image.open(self.avatar.path)
-
-        if img.height > 100 or img.width > 100:
-            new_img = (100, 100)
-            img.thumbnail(new_img)
-            img.save(self.avatar.path)
-
     def __str__(self):
+        """
+        Возвращение строки
+        """
         return self.user.username
+
+    def get_absolute_url(self):
+        """
+        Ссылка на профиль
+        """
+        return reverse('profile_detail', kwargs={'slug': self.slug})
