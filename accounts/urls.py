4a3aa745a38527cfb8064d4e2bd609187256f2b8
ms <maksimdis31@gmail.com>
2025-03-21 11:36:55 +0300

    Updating the authentication system to SIM classes, added password change via cbv

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import SignUpView, CustomLoginView, profile, ChangePasswordView
-from .views import ProfileUpdateView, ProfileDetailView
+from .views import  ChangePasswordView
+from .views import ProfileUpdateView, ProfileDetailView, UserRegisterView, UserLoginView, UserLogoutView
 
 urlpatterns = [
     path('user/edit/', ProfileUpdateView.as_view(), name='profile_edit'),
     path('user/<slug:slug>/', ProfileDetailView.as_view(), name='profile_detail'),
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-    path('profile/', profile, name='users-profile'),
+
+    # path('signup/', SignUpView.as_view(), name='signup'),
+    # path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='registration2/login.html'), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(template_name='registration2/logout.html'), name='logout'),
+    # path('profile/', profile, name='users-profile'),
+
+
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
     path('password_change/', ChangePasswordView.as_view(), name='password_change'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,96 +1,96 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView
-from .forms import SignUpForm, LoginForm, UpdateUserForm, UpdateProfileForm
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import PasswordChangeView
-from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import redirect
-from django.views import generic
-from django.contrib import messages
 from django.views.generic import DetailView, UpdateView
 from django.db import transaction
-from django.urls import reverse_lazy
 from .models import Profile
-from .forms import UserUpdateForm, ProfileUpdateForm
-
-class SignUpView(generic.CreateView):
-    form_class = SignUpForm
-    success_url = reverse_lazy("login")
-    initial = None  # принимает {'key': 'value'}
-    template_name = "registration/signup.html"
-
-    def dispatch(self, request, *args, **kwargs):
-        # перенаправит на домашнюю страницу, если пользователь попытается получить доступ к странице регистрации после авторизации
-        if request.user.is_authenticated:
-            return redirect(to='/')
-        return super().dispatch(request, *args, **kwargs)  # Добавлено
-
-    def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-
-        if form.is_valid():
-            form.save()
-
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Вы успешно зарегистрировались {username}')
-
-            return redirect(to='login')
-
-        return render(request, self.template_name, {'form': form})
-
-    # def form_valid(self, form):
-    #     response = super().form_valid(form)
-    #     messages.success(self.request, "Вы успешно зарегистрировались")
-    #     return response
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import UserLoginForm, UserRegisterForm, UserUpdateForm, ProfileUpdateForm,CustomPasswordChangeForm
 
 
-class CustomLoginView(LoginView):
-    form_class = LoginForm
 
-    def form_valid(self, form):
-        remember_me = form.cleaned_data.get('remember_me')
+# class SignUpView(generic.CreateView):
+#     form_class = SignUpForm
+#     success_url = reverse_lazy("login")
+#     initial = None  # принимает {'key': 'value'}
+#     template_name = "registration2/signup.html"
+#
+#     def dispatch(self, request, *args, **kwargs):
+#         # перенаправит на домашнюю страницу, если пользователь попытается получить доступ к странице регистрации после авторизации
+#         if request.user.is_authenticated:
+#             return redirect(to='/')
+#         return super().dispatch(request, *args, **kwargs)  # Добавлено
+#
+#     def get(self, request, *args, **kwargs):
+#         form = self.form_class(initial=self.initial)
+#         return render(request, self.template_name, {'form': form})
+#
+#     def post(self, request, *args, **kwargs):
+#         form = self.form_class(request.POST)
+#
+#         if form.is_valid():
+#             form.save()
+#
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Вы успешно зарегистрировались {username}')
+#
+#             return redirect(to='login')
+#
+#         return render(request, self.template_name, {'form': form})
 
-        if not remember_me:
-            # Установим время истечения сеанса равным 0 секундам. Таким образом, он автоматически закроет сеанс после закрытия браузера. И обновим данные.
-            self.request.session.set_expiry(0)
-            self.request.session.modified = True
+# def form_valid(self, form):
+#     response = super().form_valid(form)
+#     messages.success(self.request, "Вы успешно зарегистрировались")
+#     return response
 
-        # В противном случае сеанс браузера будет таким же как время сеанса cookie "SESSION_COOKIE_AGE", определенное в settings.py
-        return super(CustomLoginView, self).form_valid(form)
+#
+# class CustomLoginView(LoginView):
+#     form_class = LoginForm
+#
+#     def form_valid(self, form):
+#         remember_me = form.cleaned_data.get('remember_me')
+#
+#         if not remember_me:
+#             # Установим время истечения сеанса равным 0 секундам. Таким образом, он автоматически закроет сеанс после закрытия браузера. И обновим данные.
+#             self.request.session.set_expiry(0)
+#             self.request.session.modified = True
+#
+#         # В противном случае сеанс браузера будет таким же как время сеанса cookie "SESSION_COOKIE_AGE", определенное в settings.py
+#         return super(CustomLoginView, self).form_valid(form)
 
+#
+# @login_required
+# def profile(request):
+#     if request.method == 'POST':
+#         user_form = UpdateUserForm(request.POST, instance=request.user)
+#         profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+#
+#         if user_form.is_valid() and profile_form.is_valid():
+#             user_form.save()
+#             profile_form.save()
+#             messages.success(request, 'Готово')
+#             return redirect(to='users-profile')
+#     else:
+#         user_form = UpdateUserForm(instance=request.user)
+#         profile_form = UpdateProfileForm(instance=request.user.profile)
+#
+#     return render(request, 'registration2/profile.html', {'user_form': user_form, 'profile_form': profile_form})
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        user_form = UpdateUserForm(request.POST, instance=request.user)
-        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
-
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, 'Готово')
-            return redirect(to='users-profile')
-    else:
-        user_form = UpdateUserForm(instance=request.user)
-        profile_form = UpdateProfileForm(instance=request.user.profile)
-
-    return render(request, 'registration/profile.html', {'user_form': user_form, 'profile_form': profile_form})
 
 
 class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
-    template_name = 'registration/change_password.html'
-    success_message = "Successfully Changed Your Password"
-    success_url = reverse_lazy('users-profile')
+    """
+    Представление для смены пароля
+    """
+    form_class = CustomPasswordChangeForm
+    template_name = 'accounts/change_password.html'
+    success_message = "!"
+    success_url = reverse_lazy('profile_detail')
 
 
-
-
-class ProfileDetailView(LoginRequiredMixin,DetailView):
+class ProfileDetailView(LoginRequiredMixin, DetailView):
     """
     Представление для просмотра профиля
     """
@@ -106,7 +106,7 @@ class ProfileDetailView(LoginRequiredMixin,DetailView):
         return context
 
 
-class ProfileUpdateView(LoginRequiredMixin,SuccessMessageMixin,UpdateView):
+class ProfileUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     """
     Представление для редактирования профиля
     """
@@ -114,7 +114,6 @@ class ProfileUpdateView(LoginRequiredMixin,SuccessMessageMixin,UpdateView):
     form_class = ProfileUpdateForm
     template_name = 'accounts/profile_edit.html'
     success_message = 'Запись была успешно обновлена!'
-
 
     def get_object(self, queryset=None):
         return self.request.user.profile
@@ -142,3 +141,41 @@ class ProfileUpdateView(LoginRequiredMixin,SuccessMessageMixin,UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('profile_detail', kwargs={'slug': self.object.slug})
+
+
+class UserRegisterView(SuccessMessageMixin, CreateView):
+    """
+    Представление регистрации с формой регистрации
+    """
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('home')
+    template_name = 'accounts/user_register.html'
+    success_message = 'Вы успешно зарегистрировались. Можете войти на сайт!'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Регистрация на сайте'
+        return context
+
+
+class UserLoginView(SuccessMessageMixin, LoginView):
+    """
+    Авторизация
+    """
+    form_class = UserLoginForm
+    template_name = 'accounts/user_login.html'
+    next_page = 'home'
+    success_message = 'Добро пожаловать!'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Вход на сайт'
+        return context
+
+
+class UserLogoutView(LogoutView):
+    """
+    Выход с сайта
+    """
+    next_page = 'home'
+
