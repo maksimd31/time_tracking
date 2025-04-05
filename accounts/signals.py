a78e5de1acfbe203ad67b398b9f25f015900c3ed
ms <maksimdis31@gmail.com>
2025-04-05 10:56:25 +0300

    fix bug deploy

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,37 +1,3 @@
-# from django.core.mail import send_mail
-# from django.db.models.signals import post_save
-# from django.contrib.auth.models import User
-# from django.dispatch import receiver
-# from Time_tracking import settings
-# from .models import Profile
-#
-#
-# @receiver(post_save, sender=User)
-# def create_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-#
-#
-# @receiver(post_save, sender=User)
-# def user_postsave(sender, instance, created, **kwargs):
-#     if created:
-#         subject = instance.first_name
-#         message = f'Добро пожаловать {instance.username}'
-#         from_email = settings.EMAIL_HOST_USER
-#         to_email = instance.email
-#         send_mail(
-#             subject,
-#             message,
-#             from_email,
-#             [to_email],
-#             fail_silently=False
-#         )
-#
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-
 from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
@@ -49,7 +15,7 @@ def create_user_profile(sender, instance, created, **kwargs):
         profile = Profile.objects.create(user=instance)
 
         # # Генерация уникального slug
-        profile.slug = unique_slugify(profile, instance.username, 'slug')  # Укажите имя поля 'slug'
+        profile.slug = unique_slugify(profile, instance.username, 'slug')
         profile.save()
 
         # Отправка приветственного сообщения
