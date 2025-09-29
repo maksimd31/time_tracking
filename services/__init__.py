d06dbd6fe8f26434f2c82a695f558c2a3a5268ad
maksimd31 <maksimdis31@gmail.com>
2025-09-29 18:00:05 +0300

    chore: add services package __init__ to fix IDE unresolved import warnings

---
+++ services/__init__.py
@@ -0,0 +1,6 @@
+"""Service-layer utility package.
+
+This file makes IDEs treat ``services`` as a proper package so imports
+like ``from services.utils import unique_slugify`` resolve without
+inspection errors.
+"""
