fd5be4ab7e9edec746de95b5c7e708358250f68e
maksimd31 <maksimdis31@gmail.com>
2025-10-01 00:00:39 +0300

    refactor: adjust DEBUG setting to allow environment variable configuration

--- Time_tracking/settings.py
+++ Time_tracking/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY: str | None = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.getenv('DEBUG', 'False') == 'True'
+# DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 #
 # if os.getenv('ALLOWED_HOSTS'):
